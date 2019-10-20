5c51d17eadff30b2df469ad023befd5142f1d4a4
Magnus-V <magnves@gmail.com>
2019-10-20 12:35:39 +0200

    Spørs hvordan det her går.

--- js/master.js
+++ js/master.js
@@ -1,3 +1,25 @@
+var gUsername;
+
+$(document).ready(function(){
+  if(gUsername != null || gUsername != undefined){
+    $('#showName').text == gUsername;
+  }
+  else {
+    $('#showNameDiv').hide();
+  }
+});
+
 $('.washer').click(function(){
-  $('#washermodal').modal(show);
-})
+  $('#washerModal').modal('show');
+  console.log("COMEON");
+});
+
+$('#loggInnBtn').click(function(){
+  $('#loggInnModal').modal('show');
+});
+
+$('#logginnBtnConfirm').click(function(){
+  if(gUsername == null || gUsername == underfined){
+    gUsername = $('#inputUsername').val();
+  }
+});
